7f952de832b58a0b84f8cf0ca0f3e55d9ca858d8
bryan jacobs <bjacobs@incomm.com>
2019-03-20 13:19:25 -0600

    add comments

--- src/main/java/com/threads/CompletableFutureExample.java
+++ src/main/java/com/threads/CompletableFutureExample.java
@@ -16,18 +16,37 @@ public class CompletableFutureExample {
 
         int cores = Runtime.getRuntime().availableProcessors();
 
+
+        /**
+         * This ForkJoinPool is created so that the parallelStream call happens with the pool, rather than the default
+         * pool preventing slowness from not enough threads in the pool.
+         */
         ForkJoinPool pool = new ForkJoinPool(cores);
 
         var suppliers = Factory.suppliers();
 
         System.out.println("\nNumber of tasks: " + suppliers.size() + "\n");
 
-        pool.submit(() -> suppliers.parallelStream().forEach(supplier -> {
+
+        /**
+         * Important things to realize about this code.
+         *
+         * First, parallelStream() is what allows the all the different suppliers to run in parallel.  You can this
+         * test by changing suppliers.parallelStream() to suppliers.stream() and you will see that the suppliers execute
+         * in the order they are added to the List, rather than allowing shorter running suppliers to return as they
+         * complete.
+         *
+         * Second, the supplyAsync call is used so that a value can be returned from the thread.
+         */
+        Runnable runnable = () -> suppliers.parallelStream().forEach(supplier -> {
             try {
                 System.out.println(CompletableFuture.supplyAsync(supplier).get());
             } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             }
-        })).get();
+        });
+
+        // this kicks off all the fun
+        pool.submit(runnable).get();
     }
 }
